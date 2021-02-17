421f04fd25da032cbb5f26bd507c6dce9c5f72df
Eric <ericbusch@gmail.com>
2021-02-17 10:37:19 -0500

    Fixed issue with dfrcs_manage_compsets_capability filter
    Fixed issue where dfrcs_manage_compsets_capability filter wasn't allowing new capabilities to access "Add Products" page.

--- datafeedr-comparison-sets.php
+++ datafeedr-comparison-sets.php
@@ -8,7 +8,7 @@ Author URI: https://www.datafeedr.com
 License: GPL v3
 Requires at least: 3.8
 Tested up to: 5.7-beta
-Version: 0.9.46
+Version: 0.9.47
 
 WC requires at least: 3.0
 WC tested up to: 5.0
@@ -40,7 +40,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Define constants.
  */
-define( 'DFRCS_VERSION', '0.9.46' );
+define( 'DFRCS_VERSION', '0.9.47' );
 define( 'DFRCS_DB_VERSION', '0.9.0' );
 define( 'DFRCS_URL', plugin_dir_url( __FILE__ ) );
 define( 'DFRCS_PATH', plugin_dir_path( __FILE__ ) );
